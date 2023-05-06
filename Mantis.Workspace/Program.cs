455dfc206fd1218f9571633272d58089eb3680c7
Thomas Karb <thomas.karb@lshwiesentheid.onmicrosoft.com>
2023-05-06 10:42:50 +0200

    Setup of CSharp Project
    -Setup gitignore
    -Initialized Tex
    -Created Mantis.Core and Mantis.Workspace solutions

---
+++ Mantis.Workspace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mantis.Workspace
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+        }
+    }
+}
