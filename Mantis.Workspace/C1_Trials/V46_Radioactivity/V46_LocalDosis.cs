14599f5e0a3c85bf03e8bde745b1d33ccdf5dbde
ClebschGordon <salahudin.smailagic@gmail.com>
2024-03-26 12:26:40 +0100

    V46 doooooooooooooinggg

---
+++ Mantis.Workspace/C1_Trials/V46_Radioactivity/V46_LocalDosis.cs
@@ -0,0 +1,34 @@
+﻿using Mantis.Core.Calculator;
+using Mantis.Core.FileImporting;
+using Mantis.Core.QuickTable;
+using Mantis.Core.TexIntegration;
+using Mantis.Core.Utility;
+using ScottPlot.Finance;
+
+namespace Mantis.Workspace.C1_Trials.V46_Radioactivity;
+
+[QuickTable("")]
+public record struct NearFarData
+{
+    [QuickTableField("near")] public ErDouble Near;
+    [QuickTableField("far")] public ErDouble Far;
+    
+    public NearFarData()
+    {}
+}
+public class V46_LocalDosis
+{
+    public static void Process()
+    {
+        var csvReader = new SimpleTableProtocolReader("LocalDosisData.csv");
+        List<ErDouble> dataListNear = csvReader.ExtractTable<ErDouble>("tab:LocalDosisData");
+        List<ErDouble> dataListFar = csvReader.ExtractTable<ErDouble>("tab:1mDosisData");
+        
+        dataListNear.WeightedMean().AddCommandAndLog("RadiationNear" ,"ySv/h");
+    }
+
+    public static ErDouble CalculateDosis(List<ErDouble> dataList)
+    {
+        dataList.WeightedMean()
+    }
+}
---
+++ Mantis.Workspace/C1_Trials/V46_Radioactivity/V46_Main.cs
@@ -0,0 +1,12 @@
+﻿using Mantis.Core.TexIntegration;
+
+namespace Mantis.Workspace.C1_Trials.V46_Radioactivity;
+
+public class V46_Main
+{
+    public static void Process()
+    {
+        
+        TexPreamble.GeneratePreamble();
+    }
+}
